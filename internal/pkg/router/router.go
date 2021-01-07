bb81999e4563024e962af36fc78b4fd2b9a65bb9
CorentinB <corentin@archive.org>
2021-01-07 17:14:50 +0100

    Restructure API routes into groups and subgroups

--- internal/pkg/router/router.go
+++ internal/pkg/router/router.go
@@ -3,13 +3,13 @@ package router
 import (
 	"net/http"
 
-	"github.com/Lezarr/Lezarr/internal/pkg/movies"
 	"github.com/Lezarr/Lezarr/internal/pkg/setup"
+
+	"github.com/Lezarr/Lezarr/internal/pkg/movies"
+
 	"github.com/Lezarr/Lezarr/internal/pkg/system"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
-	ginSwagger "github.com/swaggo/gin-swagger"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 // Init initialise the router and the routes, and return it
@@ -23,14 +23,20 @@ func Init(config *setup.Config) *gin.Engine {
 		c.String(http.StatusOK, "Hello world!")
 	})
 
-	url := ginSwagger.URL("http://localhost:2343/swagger/doc.json")
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+	api := router.Group("/api")
+	{
+		moviesAPI := api.Group("/movies")
+		{
+			moviesAPI.GET("/search", movies.Search(config.Database, config.TMDBClient))
+			moviesAPI.POST("/add", movies.Add(config.Database, config.TMDBClient))
+			moviesAPI.GET("/get", movies.Get(config.Database, config.TMDBClient))
+		}
 
-	router.GET("/api/movies/search", movies.Search(config.Database, config.TMDBClient))
-	router.POST("/api/movies/add", movies.Add(config.Database, config.TMDBClient))
-	router.GET("/api/movies/get", movies.Get(config.Database, config.TMDBClient))
-
-	router.GET("/api/system/stats", system.GetStats(config.Database))
+		systemAPI := api.Group("/system")
+		{
+			systemAPI.GET("/stats", system.GetStats(config.Database))
+		}
+	}
 
 	return router
 }
